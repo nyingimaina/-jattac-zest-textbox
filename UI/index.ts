803e940ea46cfcc8df0e16f70f47d2e542fb328b
= <=>
2026-02-04 00:29:07 +0300

    lucas

--- UI/index.ts
+++ UI/index.ts
@@ -1,7 +1,9 @@
-export { default as ZestTextbox } from './ZestTextbox';
-export type { ZestTextboxSize, ZestProps, HelperTextConfig } from './types';
-export { PasswordToggleButton } from './components/PasswordToggleButton';
-export { CharacterCounter } from './components/CharacterCounter';
-export { ProgressBar } from './components/ProgressBar';
-export { HelperTextDisplay } from './components/HelperTextDisplay';
-export { ZestTextboxConfigProvider } from './contexts/ZestTextboxConfigContext';
+import ZestTextbox from "./ZestTextbox";
+
+export { ZestTextbox as default };
+export type { ZestTextboxSize, ZestProps, HelperTextConfig } from "./types";
+export { PasswordToggleButton } from "./components/PasswordToggleButton";
+export { CharacterCounter } from "./components/CharacterCounter";
+export { ProgressBar } from "./components/ProgressBar";
+export { HelperTextDisplay } from "./components/HelperTextDisplay";
+export { ZestTextboxConfigProvider } from "./contexts/ZestTextboxConfigContext";
